8316592ea46d482333c1a0b3a96600e03b2f6786
Omer <omerkaraman@hotmail.co.uk>
2021-04-02 13:47:04 +0100

    changed page title

--- gan_2/web_viewer/src/index.js
+++ gan_2/web_viewer/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-document.title="FashionGAN"
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
